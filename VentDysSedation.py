526c2ce635d9e2cd4b54c157d1e70b0bb76f7e96
Peter D Sottile <psottile05@gmail.com>
2015-10-20 20:11:18 -0600

    refactor to imrpove adding breath settings data

--- VentDysSedation.py
+++ VentDysSedation.py
@@ -12,9 +12,7 @@ import json
 import pandas as pd
 import pymongo
 import datetime
-import re
 import gevent
-import numba
 from gevent.lock import Semaphore
 
 pd.set_option('max_columns', 40)
@@ -46,7 +44,7 @@ FS.file_search()
 FS.file_match()
 
 # Query DB for list of files not yet added
-files = list(input_log.find({'type': 'waveform', 'loaded': 0}).limit(3))
+files = list(input_log.find({'type': 'waveform', 'loaded': 0}).limit(5))
 
 def get_waveform_and_breath(file, semaphore):
     with semaphore:
