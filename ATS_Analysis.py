1a46cf40f2cd4f9960a11722034fb2f3054740f2
Peter D Sottile <psottile05@gmail.com>
2015-10-27 16:45:12 -0600

    ATS analysis

--- ATS_Analysis.py
+++ ATS_Analysis.py
@@ -38,7 +38,7 @@ def collection_freq(breath_df, win):
         breath_df[ds_type + '_tot_rolling'] = pd.rolling_count(breath_df['analysis.' + ds_type], window = 60 * win,
                                                               center = True)
         breath_df[ds_type + '_freq'] = breath_df[ds_type + '_rolling'] / breath_df[ds_type + '_tot_rolling']
-        print(breath_df.columns)
+
     return breath_df
 
 
