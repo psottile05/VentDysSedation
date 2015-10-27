608cc451f96b90d91841b7d204cb10a36404d0e5
Peter D Sottile <psottile05@gmail.com>
2015-10-26 18:18:28 -0600

    ATS analysis

--- ATS_Analysis.py
+++ ATS_Analysis.py
@@ -14,14 +14,14 @@ def data_collect(patient):
     rn_data = db.RNData_collection
 
     results = breath_data.find({'patientID': patient},
-                               {'_id': 0, 'patientID': 1, 'start_time': 1, 'analysis': 1}).limit(1)
+                               {'_id': 0, 'patientID': 1, 'start_time': 1, 'analysis': 1})
     breath_df = pd.io.json.json_normalize(results)
     breath_df.set_index(['start_time'], inplace = True)
     breath_df['patientID'] = breath_df['patientID'].str.lstrip('P').astype(int)
-    breath_df.resample('1s', limit = 120)
+    breath_df = breath_df.resample('1s', limit = 20)
 
     results = rn_data.find({'patientID': patient, 'RN_entry.RASS': {'$exists': 1}},
-                           {'_id': 0, 'patientID': 1, 'entry_time': 1, 'RN_entry': 1}).limit(10)
+                           {'_id': 0, 'patientID': 1, 'entry_time': 1, 'RN_entry': 1})
     rn_df = pd.DataFrame.from_dict(list(results))
     rn_df.set_index(['entry_time'], inplace = True)
     rn_df['patientID'] = rn_df['patientID'].str.lstrip('P').astype(int)
@@ -31,12 +31,32 @@ def data_collect(patient):
 
 
 def collection_freq(breath_df, win):
-    breath_df['ds_rolling'] = pd.rolling_sum(breath_df['analysis.ds'], window = 60 * win)
-    breath_df['tot_rolling'] = pd.rolling_count(breath_df['analysis.ds'], window = 60 * win)
+    breath_df['ds_rolling'] = pd.rolling_sum(breath_df['analysis.ie'], window = 60 * win, min_periods = 1)
+    breath_df['tot_rolling'] = pd.rolling_count(breath_df['analysis.ie'], window = 60 * win)
     breath_df['ds_freq'] = breath_df.ds_rolling / breath_df.tot_rolling
 
     return breath_df
 
 
-breath_df, rn_df = data_collect('P104')
+def rolling_rass_combi(breath_df, rn_df):
+    combi_df = rn_df.join(breath_df, how = 'left', lsuffix = '_l')
+    combi_df.drop(
+        ['analysis.ds', 'analysis.fl', 'analysis.ie', 'analysis.pl', 'analysis.pvt', 'RN_entry', 'patientID_l'],
+        axis = 1, inplace = True)
+    combi_df.dropna(axis = 0, how = 'any', subset = ['ds_freq'], inplace = True)
+    return combi_df
+
+
+'''
+breath_df, rn_df = data_collect('P110')
 breath_df = collection_freq(breath_df, 10)
+combi_df = rolling_rass_combi(breath_df, rn_df)
+
+
+print(combi_df)
+print(pd.notnull(combi_df['ds_freq']).any())
+print(combi_df['ds_freq'].count())
+
+print(rn_df['RASS'].count())
+print(rn_df[(rn_df.index >= breath_df.index.min())and(rn_df.index <= breath_df.index.max())]['RASS'].count())
+'''
