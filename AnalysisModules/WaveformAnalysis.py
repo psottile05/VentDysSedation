18f7b4a793b984bfc3101c8736ad8b70d095f43a
Peter D Sottile <psottile05@gmail.com>
2015-11-22 15:23:10 -0700

    adding waveform analysis

---
+++ AnalysisModules/WaveformAnalysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import scipy.signal as sig
+import numba
+
+
+def find_min():
+    pass
+
+
+def find_max():
+    pass
+
+# Insp Min/Max
+# Exp Min/Max
