c883f4fd6a71bd9b863395a01775266411efc184
Peter D Sottile <psottile05@gmail.com>
2015-11-23 13:57:18 -0700

    adding waveform analysis

--- VentDysSedation.py
+++ VentDysSedation.py
@@ -11,7 +11,7 @@ from CreationModules import FileSearch as FS
 
 ipclient = Client()
 print(ipclient.ids)
-ipview = ipclient.load_balanced_view()
+ipview = ipclient.direct_view()
 
 client = MongoClient()
 db = client.VentDB
@@ -41,27 +41,27 @@ FS.file_match()
 
 @ipview.parallel(block = True)
 def make_waveform_and_breath(files):
-    import CreationModules.DatabaseCreation as DBCreate
+    from CreationModules import DatabaseCreation
     for file in files:
-        DBCreate.get_waveform_and_breath(file)
+        DatabaseCreation.get_waveform_and_breath(file)
 
 
 @ipview.parallel(block = True)
 def make_EHR_data(files):
-    import CreationModules.EHR_decoder as EHR
+    from CreationModules import EHR_decoder
     for file in files:
-        EHR.load_EHR_data(file['_id'], file['patient_id'])
+        EHR_decoder.load_EHR_data(file['_id'], file['patient_id'])
 
 
 # Query DB for list of Waveform/breath files not yet added
-files = list(input_log.find({'type': 'waveform', 'loaded': 0}).limit(1))
+files = list(input_log.find({'type': 'waveform', 'loaded': 0}).limit(3))
 make_waveform_and_breath(files)
 
 # Query DB for list of EHR files not yet added
 files = list(input_log.find({'$and': [{'type': {'$not': re.compile(r'waveform')}},
                                       {'type': {'$not': re.compile(r'breath')}},
                                       {'type': {'$not': re.compile(r'other')}},
-                                      {'loaded': 0}]}, {'_id': 1, 'patient_id': 1}).limit(1))
+                                      {'loaded': 0}]}, {'_id': 1, 'patient_id': 1}).limit(3))
 make_EHR_data(files)
 
 for items in input_log.find({'loaded': 1}, {'type': 1}):
