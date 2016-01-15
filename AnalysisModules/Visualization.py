a62aefa3a6a48770facd2897bd4717ec7db8d758
Peter D Sottile <psottile05@gmail.com>
2016-01-15 15:10:23 -0700

    add error logging visualizations with plotly

--- AnalysisModules/Visualization.py
+++ AnalysisModules/Visualization.py
@@ -2,7 +2,6 @@ import pandas as pd
 from pymongo import MongoClient
 from bokeh.plotting import figure, output_notebook, show, vplot
 
-
 client = MongoClient()
 db = client.VentDB
 breath_db = db.breath_collection
@@ -42,14 +41,41 @@ def breath_viz(id):
     df = pd.concat([pd.DataFrame(pre_breath['breath_raw']), pd.DataFrame(breath['breath_raw']),
                     pd.DataFrame(post_breath['breath_raw'])])
 
-    return df
+    return df, breath_start, breath_end
 
 
-def make_plot(df):
-    p1, p2, p3 = figure(plot_height = 200)
-    p1.line(df['time'], df['sm_flow'], color = 'firebrick')
-    p2.line(df['time'], df['sm_paw'], color = 'navy')
-    p3.line(df['time'], df['sm_vol'], color = 'olive')
+def make_plot(df, breath_start, breath_end):
+    p1, p2, p3 = [figure(plot_height = 250, toolbar_location = 'right') for x in range(3)]
+    p1.line(df['time'], df['sm_flow'], color = 'firebrick', name = 'Flow')
+    p1.line([breath_start, breath_start], [-40, 40], color = 'green')
+    p1.line([breath_end, breath_end], [-40, 40], color = 'green')
+    p1.yaxis.axis_label = 'Flow'
+
+    p2.line(df['time'], df['sm_paw'], color = 'navy', name = 'Paw')
+    p2.line([breath_start, breath_start], [0, 40], color = 'green')
+    p2.line([breath_end, breath_end], [0, 40], color = 'green')
+    p2.yaxis.axis_label = 'Paw'
+
+    p3.line(df['time'], df['sm_vol'], color = 'olive', name = 'Volume')
+    p3.line([breath_start, breath_start], [0, 400], color = 'green')
+    p3.line([breath_end, breath_end], [0, 400], color = 'green')
+    p3.yaxis.axis_label = 'Volume'
 
     p = vplot(p1, p2, p3)
     return p
+
+
+def update_database(_id, labels):
+    analysis = {'norm': 1 if 'Normal' in labels else 0,
+                'ds': 1 if 'DoubleStacked' in labels else 0,
+                'pds': 1 if 'PostDoubleStacked' in labels else 0,
+                'pfl': 1 if 'PressureFlowLimited' in labels else 0,
+                'fa': 1 if 'FlowAbnormal' in labels else 0,
+                'pvt': 1 if 'PrematureVentTermination' in labels else 0,
+                'ie': 1 if 'IneffectiveTrigger' in labels else 0,
+                'garb': 1 if 'Garbage' in labels else 0,
+                }
+
+    breath_db.update({'_id': _id}, {'$set': {'validation': analysis}})
+
+    return
