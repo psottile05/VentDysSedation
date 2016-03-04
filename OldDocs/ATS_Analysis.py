b7a5a420e4db15d39503cc39120799ed579d671a
Peter D Sottile <psottile05@gmail.com>
2016-03-04 15:51:13 -0700

    breath analysis

--- OldDocs/ATS_Analysis.py
+++ OldDocs/ATS_Analysis.py
@@ -45,7 +45,7 @@ def data_collect(patient, patient_info):
         rn_df['SpO2'] = rn_df['RN_entry'].apply(lambda x: unpack_entry(x, 'SpO2'))
         rn_df.drop(['RN_entry'], axis = 1, inplace = True)
 
-    print('info', patient_info)
+    print('info', patient_info['NMB'])
     if patient_info['NMB'] == 1:
         start_stop = patient_info['Start_End_NMB'].strip('[]').split('), (')
 
