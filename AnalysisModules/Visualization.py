5ce850675f588e76623e017a743b5f3291c376f7
Peter D Sottile <psottile05@gmail.com>
2016-03-09 21:15:56 -0700

    breath analysis

--- AnalysisModules/Visualization.py
+++ AnalysisModules/Visualization.py
@@ -24,32 +24,41 @@ def get_breaths(limits):
 
 
 def breath_viz(id):
-    breath = list(breath_db.find({'_id': id}, {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
-    breath_start = breath['breath_raw']['time'][0]
-    breath_end = breath['breath_raw']['time'][-1]
-
     try:
-        pre_breath = list(breath_db.find({'file': breath['file'], 'breath_num': breath['breath_num'] - 1},
-                                         {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
-    except IndexError:
-        pre_breath = {
-            'breath_raw': {'flow': [], 'sm_dV/dTT': [], 'vol': [], 'dF/dT': [], 'sm_vol': [], 'breath': [], 'time': [],
-                           'sm_dF/dT': [], 'sm_flow': [], 'status': [], 'sm_dF/dTT': [], 'sm_dV/dT': [], 'dP/dV': [],
-                           'paw': [], 'sm_paw': [], 'sm_dP/dT': [], 'dF/dV': [], 'dP/dT': [], 'dV/dT': [], 'dF/dP': [],
-                           'sm_dP/dTT': []}}
+        breath = list(breath_db.find({'_id': id}, {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
+        breath_start = breath['breath_raw']['time'][0]
+        breath_end = breath['breath_raw']['time'][-1]
 
-    try:
-        post_breath = list(breath_db.find({'file': breath['file'], 'breath_num': breath['breath_num'] + 1},
-                                          {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
-    except IndexError:
-        post_breath = {
-            'breath_raw': {'flow': [], 'sm_dV/dTT': [], 'vol': [], 'dF/dT': [], 'sm_vol': [], 'breath': [], 'time': [],
-                           'sm_dF/dT': [], 'sm_flow': [], 'status': [], 'sm_dF/dTT': [], 'sm_dV/dT': [], 'dP/dV': [],
-                           'paw': [], 'sm_paw': [], 'sm_dP/dT': [], 'dF/dV': [], 'dP/dT': [], 'dV/dT': [], 'dF/dP': [],
-                           'sm_dP/dTT': []}}
+        try:
+            pre_breath = list(breath_db.find({'file': breath['file'], 'breath_num': breath['breath_num'] - 1},
+                                             {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
+        except IndexError:
+            pre_breath = {
+                'breath_raw': {'flow': [], 'sm_dV/dTT': [], 'vol': [], 'dF/dT': [], 'sm_vol': [], 'breath': [],
+                               'time': [],
+                               'sm_dF/dT': [], 'sm_flow': [], 'status': [], 'sm_dF/dTT': [], 'sm_dV/dT': [],
+                               'dP/dV': [],
+                               'paw': [], 'sm_paw': [], 'sm_dP/dT': [], 'dF/dV': [], 'dP/dT': [], 'dV/dT': [],
+                               'dF/dP': [],
+                               'sm_dP/dTT': []}}
 
-    df = pd.concat([pd.DataFrame(pre_breath['breath_raw']), pd.DataFrame(breath['breath_raw']),
-                    pd.DataFrame(post_breath['breath_raw'])])
+        try:
+            post_breath = list(breath_db.find({'file': breath['file'], 'breath_num': breath['breath_num'] + 1},
+                                              {'file': 1, 'breath_num': 1, 'breath_raw': 1}))[0]
+        except IndexError:
+            post_breath = {
+                'breath_raw': {'flow': [], 'sm_dV/dTT': [], 'vol': [], 'dF/dT': [], 'sm_vol': [], 'breath': [],
+                               'time': [],
+                               'sm_dF/dT': [], 'sm_flow': [], 'status': [], 'sm_dF/dTT': [], 'sm_dV/dT': [],
+                               'dP/dV': [],
+                               'paw': [], 'sm_paw': [], 'sm_dP/dT': [], 'dF/dV': [], 'dP/dT': [], 'dV/dT': [],
+                               'dF/dP': [],
+                               'sm_dP/dTT': []}}
+
+        df = pd.concat([pd.DataFrame(pre_breath['breath_raw']), pd.DataFrame(breath['breath_raw']),
+                        pd.DataFrame(post_breath['breath_raw'])])
+    except:
+        pass
 
     return df, breath_start, breath_end
 
