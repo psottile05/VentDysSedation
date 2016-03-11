2a089a32110d9fde0a0c90128603e818319d75f2
Peter D Sottile <psottile05@gmail.com>
2016-03-10 18:05:40 -0700

    breath analysis

--- AnalysisModules/Visualization.py
+++ AnalysisModules/Visualization.py
@@ -9,11 +9,12 @@ train_db = db.train_collection
 
 
 def get_sample():
-    if train_db.find().count() != 10000:
+    if train_db.find().count() < 10000:
+        answ = input('Are you sure you want to drop the training set?? (y/n)')
+        if answ = 'y'
         db.drop_collection('train_collection')
         print('making train_collection')
-        breath_db.aggregate([  # {'$match': {'patient_id': {'$lt': 115}}},
-            # {'$project': {'_id': 1}},
+        breath_db.aggregate([{'$match': {'patient_id': {'$lt': 115}}},
                              {'$sample': {'size': 10000}},
                              {'$out': 'train_collection'}])
 
