d6526276695aa7317744c43b10bc0488e78018e6
Peter D Sottile <psottile05@gmail.com>
2016-04-07 11:21:08 -0600

    error corrections

--- CreationModules/PriorBreathData.py
+++ CreationModules/PriorBreathData.py
@@ -8,14 +8,10 @@ breath_col = db.breath_collection
 
 
 def update_breath(current_breath):
-    # current_breath_list = breath_col.find({'next_breath_data': {'$exists': 0}},
-    #                                      {'patient_id': 1, 'file': 1, 'breath_num': 1, 'breath_character': 1})
-
-    #for current_breath in current_breath_list:
-        next_breath = breath_col.find_one({'patient_id': current_breath['patient_id'],
+    next_breath = breath_col.find_one({'patient_id': current_breath['patient_id'],
                                            'file': current_breath['file'],
                                            'breath_num': current_breath['breath_num'] + 1},
-                                          {'_id': 0, 'breath_character': 1})
+                                      {'_id': 0, 'breath_character': 1})
 
     if not isinstance(next_breath, type(None)):
             breath_col.find_one_and_update({'patient_id': current_breath['patient_id'],
